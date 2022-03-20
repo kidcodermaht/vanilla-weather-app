29f5785d7773a61660223723b672c506b287f598
kidcodermaht <walkthroughfaya@gmail.com>
2022-03-20 15:25:55 +0330

    removed all the logs

--- src/app.js
+++ src/app.js
@@ -61,7 +61,6 @@ function displayForcast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "b761a023f32606217de81af7721e0f32";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
